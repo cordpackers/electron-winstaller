1d0e0a0639b891527a552f83fa565db1e6e7d650
Deskehs <124361319+Deskehs@users.noreply.github.com>
2024-08-11 01:35:51 +0800

    change template

--- src/index.ts
+++ src/index.ts
@@ -262,14 +262,12 @@ export async function createWindowsInstaller(options: SquirrelWindowsOptions): P
   if (options.fixUpPaths !== false) {
     log('Fixing up paths');
 
-    /*
     if (metadata.productName || options.setupExe) {
       const setupPath = path.join(outputDirectory, options.setupExe || `${metadata.productName}Setup.exe`);
       const unfixedSetupPath = path.join(outputDirectory, 'Setup.exe');
       log(`Renaming ${unfixedSetupPath} => ${setupPath}`);
       await fs.rename(unfixedSetupPath, setupPath);
     }
-    */
 
     if (metadata.productName || options.setupMsi) {
       const msiPath = path.join(outputDirectory, options.setupMsi || `${metadata.productName}Setup.msi`);
